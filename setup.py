4703618f06ca316d3bd6c97f6ff4f1cffc7047d9
Chris Coetzee <chriscz93@gmail.com>
2017-02-17 14:38:33 +0200

    Fix setup URL

--- setup.py
+++ setup.py
@@ -3,15 +3,14 @@ import sys
 
 from setuptools import setup, find_packages
 
-VERSION = '0.2.0'
+VERSION = '0.2.1'
 
 setup(
     name='funciter',
     version=VERSION,
     description='Functional style iterators and iterable manipulation',
-    #long_description=open(os.path.join(base_dir, 'description.txt')).read().strip(),
     license='Mozilla Public License 2.0 (MPL 2.0)',
-    url='https://github.com/chriscz/pyfuncty',
+    url='https://github.com/chriscz/funciter',
 
     author='Chris Coetzee',
     author_email='chriscz93@gmail.com',
